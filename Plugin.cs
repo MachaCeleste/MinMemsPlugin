d46963924d320970f9f9af8def5fc548ced5dc6f
Macha Celeste <shetah1@yahoo.com>
2025-05-01 12:10:50 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace MinMemsPlugin;
+
+[BepInPlugin("com.machaceleste.minmemsplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> minMems;
+
+    private void Awake()
+    {
+        minMems = Config.Bind("Main", "Min Mems", 2, new ConfigDescription("Sets the min number of memory addresses per library, must be set before singleplayer launches, default: 2", new AcceptableValueRange<int>(1, 5)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.minmemsplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/LibraryPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MinMemsPlugin;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Util;
+
+[HarmonyPatch]
+public class LibraryPatch
+{
+    [HarmonyPatch(typeof(Library), "Configure")]
+    class ConfigurePatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (Networking.IsSinglePlayer() && instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 2)
+                    instruction.operand = Plugin.minMems.Value;
+                yield return instruction;
+            }
+        }
+    }
+}
